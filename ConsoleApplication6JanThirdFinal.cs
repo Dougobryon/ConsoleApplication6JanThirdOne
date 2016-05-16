5816d3ad362cb2068e515906f9224f46cd6c0580
Your Name <you.example.com>
2016-05-16 14:04:41 -0400

    6JanThirdOne

---
+++ ConsoleApplication6JanThirdFinal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6JanThirdOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] pointvalues = { 2.2, 4.4, 6.6, 8.8, 9.9  };
+            foreach (double pointvalue in pointvalues)
+            {
+                Console.Write("{0} ", pointvalue.ToString("N1"));
+            }           
+        }
+    }
+}
